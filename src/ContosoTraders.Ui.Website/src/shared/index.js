af26ddbd576ccee03b652513f243e4d7c126a763
mithun shanbhag <mithunshanbhag@hotmail.com>
2022-12-03 15:05:53 +0530

    initial commit: contosotraders

---
+++ src/ContosoTraders.Ui.Website/src/shared/index.js
@@ -0,0 +1,19 @@
+import Header from './header/header';
+import Appbar from './header/appbar';
+import Categories from './header/categories';
+import DebugHeader from './debugHeader/debugHeader';
+import Footer from './footer/footer';
+import LoadingSpinner from './loadingSpinner/loadingSpinner'
+import UploadFile from './uploadFile/uploadFile'
+import Card from './card/card'
+
+export {
+    Header,
+    Appbar,
+    DebugHeader,
+    Footer,
+    LoadingSpinner,
+    UploadFile,
+    Card,
+    Categories,
+};
